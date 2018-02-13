fd0430e4f056ec7f10afaaabdde492ab09e3d900
wi34rd <wi34rd@gmail.com>
2018-02-13 07:57:06 +0700

    Добавил решение задачи Find a string раздела Strings

---
+++ HackerRank/Python/03 Strings/p05_find_a_string.py
@@ -0,0 +1,19 @@
+# Find a string
+# https://www.hackerrank.com/challenges/find-a-string/problem
+
+def count_substring(string, sub_string):
+    counter = 0
+    
+    for i in range(len(string) - len(sub_string) + 1):
+        if string[i:i+len(sub_string)] == sub_string:
+            counter += 1
+
+    return counter
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
