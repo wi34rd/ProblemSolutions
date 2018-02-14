16d30fda26a3d1ff5b07e2371b1e665029c2066a
wi34rd <wi34rd@gmail.com>
2018-02-14 11:41:28 +0700

    Добавил решение задачи String Formatting раздела Strings

---
+++ HackerRank/Python/03 Strings/p10_string_formatting.py
@@ -0,0 +1,12 @@
+# String Formatting
+# https://www.hackerrank.com/challenges/designer-door-mat/problem
+
+def print_formatted(number):
+    for i in range(1, number + 1):
+        print('{1:{0}} {2:{0}o} {3:{0}X} {4:{0}b}'.format(len(bin(n)) - 2, i, i, i, i))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    print_formatted(n)
