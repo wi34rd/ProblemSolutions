622bf1eeecdddafd3eeb4d0854de2006dd835223
wi34rd <wi34rd@gmail.com>
2018-02-15 11:24:13 +0700

    Добавил решение задачи Introduction to Sets раздела Sets

---
+++ HackerRank/Python/04 Sets/p01_introduction_to_sets.py
@@ -0,0 +1,14 @@
+# Introduction to Sets
+# https://www.hackerrank.com/challenges/py-introduction-to-sets/problem
+
+def average(array):
+    array = set(array)
+    
+    return sum(array) / len(array)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
