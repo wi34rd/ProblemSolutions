38685ebdb575607ac55a2605cb574650897f0b79
wi34rd <wi34rd@gmail.com>
2018-02-16 13:51:37 +0700

    Добавил решение задачи Set .difference() Operation раздела Sets

---
+++ HackerRank/Python/04 Sets/p08_set_difference_operation.py
@@ -0,0 +1,9 @@
+# Set .difference() Operation
+# https://www.hackerrank.com/challenges/py-set-difference-operation/problem
+
+_ = input()
+set1 = set(input().split())
+_ = input()
+set2 = set(input().split())
+
+print(len(set1 - set2))
