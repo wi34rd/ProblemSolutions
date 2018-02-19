5a79eae02835cac6f808151387d69a04961bf539
wi34rd <wi34rd@gmail.com>
2018-02-19 17:43:04 +0700

    Решена задача Check Strict Superset раздела Sets

---
+++ HackerRank/Python/04 Sets/p13_check_strict_superset.py
@@ -0,0 +1,6 @@
+# Check Strict Superset
+# https://www.hackerrank.com/challenges/py-check-strict-superset/problem
+
+A = set(input().split())
+
+print(all(A > set(input().split()) for _ in range(int(input()))))
