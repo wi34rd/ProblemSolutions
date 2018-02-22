ffc59674b12078cd65af520fba548e1df9da396e
wi34rd <wi34rd@gmail.com>
2018-02-22 21:04:25 +0700

    Решена задача Maximize It! раздела Itertools

---
+++ HackerRank/Python/06 Itertools/p07_maximize_it.py
@@ -0,0 +1,10 @@
+# Maximize It!
+# https://www.hackerrank.com/challenges/maximize-it/problem
+
+from itertools import product
+
+
+K, M = map(int, input().split())
+
+print(max(map(lambda x: sum(i ** 2 for i in x) % M,
+              product(*(list(map(int, input().split()))[1:] for _ in range(K))))))
