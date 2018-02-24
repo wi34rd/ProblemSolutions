4104a41bd9d71025236635a3de3d5ee607661525
wi34rd <wi34rd@gmail.com>
2018-02-24 20:22:28 +0700

    Решена задача Следующее и предыдущее раздела Ввод и вывод данных

---
+++ Питонтьютор/01. Ввод и вывод данных/p06_sleduyuschee_i_prediduschee.py
@@ -0,0 +1,6 @@
+# Следующее и предыдущее
+# http://pythontutor.ru/lessons/inout_and_arithmetic_operations/problems/next_and_previous/
+
+number = int(input())
+
+print('The next number for the number %s is %s.\nThe previous number for the number %s is %s.' % (number, number + 1, number, number - 1))
